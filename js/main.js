c112e45f150dfa45d15c8987d41813f19a2ad818
chichichi2203 <93566378+chichichi2203@users.noreply.github.com>
2021-11-22 17:09:17 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
     var offsetY = $("#loveHeart").height() / 2 - 55;
     // Cài đặt thời gian
     var together = new Date();
-    together.setFullYear(2021, 05, 13);
+    together.setFullYear(2021, 04, 13);
     together.setHours(20);
     together.setMinutes(0);
     together.setSeconds(0);
@@ -19,7 +19,7 @@
     if (!document.createElement('canvas').getContext) {
         var msg = document.createElement("div");
         msg.id = "errorMsg";
-        //msg.innerHTML = "您的浏览器不支持 HTML5!<br/>推荐使用 Chrome 14+/IE 9+/Firefox 7+/Safari 4+ 等高版本的主流浏览器";
+        msg.innerHTML = "您的浏览器不支持 HTML5!<br/>推荐使用 Chrome 14+/IE 9+/Firefox 7+/Safari 4+ 等高版本的主流浏览器";
         document.body.appendChild(msg);
         $("#code").css("display", "none")
         $("#copyright").css("position", "absolute");
